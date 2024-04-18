f31c1ffe98efb20a020b15195e2e93d3ef39449e
linnkhant07 <lkty07@gmail.com>
2024-04-18 11:41:48 -0700

    embedded canvas

--- build/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/linnkhantthuya/Documents/CS_Projects/exam-grader/build/_deps/googletest-src
-# Build directory: /Users/linnkhantthuya/Documents/CS_Projects/exam-grader/build/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
