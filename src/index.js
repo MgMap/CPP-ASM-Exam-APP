efadbcef21da3b714f2705385d52c03317f9e92e
MgMap <minaungpaing.pp@gmail.com>
2024-08-09 08:42:40 -0700

    browserView

--- src/index.js
+++ src/index.js
@@ -1,218 +1,130 @@
 const { app, BrowserWindow, BrowserView, ipcMain, dialog } = require('electron');
-const path = require('node:path');
+const path = require('path');
 const fs = require('fs');
-
-//for disabling shortcuts
-const { globalShortcut } = require('electron');
-const { compileCpp } = require('./compile');
 
 let mainWindow;
 let canvasView;
+let editorView;
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   app.quit();
 }
 
-function createCanvasView() {
-  // Create a BrowserView for the canvas
-  canvasView = new BrowserView();
-
-  // Set the web contents of the BrowserView to the Canvas webpage
-  canvasView.webContents.loadURL('https://canvas.pasadena.edu');
-
-  // Attach the BrowserView to the main window
-  mainWindow.setBrowserView(canvasView);
-
-  // Set the bounds of the BrowserView
-  canvasView.setBounds({ x: 32, y: 215, width: 0, height: 0 });
-
-
-  // Store a reference to the canvas BrowserView in the main window object
-  mainWindow.canvasView = canvasView;
-}
-
 const createWindow = () => {
-  // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 1600,
-
     height: 1000,
-    //alwaysOnTop: true,
-      kiosk: false, //comment this out if it is too annoying
-      webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
+    webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: false,
+      nodeIntegration: true,
     },
   });
 
-  // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
-  // Alert when the user tries to close the app
   mainWindow.on('close', (event) => {
-    
-    event.preventDefault(); // Prevent the window from closing
+    event.preventDefault();
     dialog.showMessageBox(mainWindow, {
-    type: 'warning',
-    message: 'Are you sure you want to exit the exam?',
-    buttons: ['Yes', 'No'],
-    defaultId: 1,
-    cancelId: 1
-  }).then((response) => {
-    if (response.response === 0) {
-      // User clicked 'Yes', close the window
-      mainWindow.destroy();
-    }
+      type: 'warning',
+      message: 'Are you sure you want to exit the exam?',
+      buttons: ['Yes', 'No'],
+      defaultId: 1,
+      cancelId: 1,
+    }).then((response) => {
+      if (response.response === 0) {
+        mainWindow.destroy();
+      }
+    });
   });
-});
 
-mainWindow.on('blur', () => {
+  mainWindow.on('blur', () => {
+    mainWindow.webContents.send('blur-app');
+  });
 
-  mainWindow.webContents.send('blur-app');
-});
+  mainWindow.on('focus', () => {
+    mainWindow.webContents.send('focus-app');
+  });
 
-// Track when the app gains focus
-mainWindow.on('focus', () => {
-  // User returned to the app
-  mainWindow.webContents.send('focus-app');
-  //notificationShown = false;
-});
-  // Create and attach the canvas BrowserView
-  createCanvasView();
+  // Initialize the views but don't add them yet
+  canvasView = new BrowserView({
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: false,
+      nodeIntegration: true,
+    },
+  });
+  canvasView.webContents.loadURL('https://canvas.pasadena.edu');
 
-  // Listen for IPC messages from the renderer process
-  ipcMain.on('show-canvas', (event,arg) => {
+  editorView = new BrowserView({
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: false,
+      nodeIntegration: true,
+    },
+  });
+  editorView.webContents.loadURL(path.join(__dirname, 'editor.html')).then(() => {
+    console.log('Editor view loaded successfully.');
+  }).catch((err) => {
+    console.error('Failed to load editor view:', err);
+  });
 
+  ipcMain.on('show-canvas', () => {
+    if (mainWindow.getBrowserView() === editorView) {
+      mainWindow.removeBrowserView(editorView);
+    }
+    mainWindow.addBrowserView(canvasView);
     canvasView.setBounds({ x: 32, y: 215, width: 1000, height: 770 });
-    //x=32, y:215
   });
 
   ipcMain.on('show-text-editor', () => {
-      // Logic to show the Text Editor window
-      canvasView.setBounds({ x: 0, y: 0, width: 1, height: 1 });
+    if (mainWindow.getBrowserView() === canvasView) {
+      mainWindow.removeBrowserView(canvasView);
+    }
+    mainWindow.addBrowserView(editorView);
+    editorView.setBounds({ x: 32, y: 215, width: 1000, height: 770 });
+    console.log("text-editor-index");
+    mainWindow.webContents.send('show-text-editor');
   });
 
-  // Alert when the user tries to exit the app
-  ipcMain.on('exit-app', (event) => {
+  ipcMain.on("please", ()=>
+  {
+    console.log("from editor.html");
+  });
+  ipcMain.on('fetch-file', (event) => {
+    const filePath = path.join(__dirname, '../_tests/_test_files/basic_test.cpp');
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        console.error('Failed to read file:', err);
+        return;
+      }
+      editorView.webContents.send('fetch-file', data);
+    });
+  });
+
+  ipcMain.on('compile-code', (event, code) => {
+    console.log('Compiling code:', code);
+    editorView.webContents.send('compile-result', 'Compilation successful!');
+  });
+
+  ipcMain.on('exit-app', () => {
     mainWindow.destroy();
   });
 
-  // Open the DevTools.
   mainWindow.webContents.openDevTools();
-
-
-ipcMain.on('save-test-B', (event,code)=>{
-  const filePath = path.join(__dirname, '../_tests/_test_files/testB.cpp');
-  fs.writeFile(filePath, code, (err) => {
-    if (err) {
-        console.error('Failed to save file', err);
-        event.reply('save-testB-reply', 'fail');
-        return;
-    }
-    console.log('File saved successfully');
-    event.reply('save-testB-reply', 'success');
-
-    // Run the compile function
-});
-});
-
-ipcMain.on('save-student-file', (event,code)=>{
-  const filePath = path.join(__dirname, '../includes/poly/poly_student.cpp');
-  fs.writeFile(filePath, code, (err) => {
-    if (err) {
-        console.error('Failed to save file', err);
-        event.reply('save-student-file-reply', 'fail');
-        return;
-    }
-    console.log('File saved successfully');
-    event.reply('save-student-file-reply', 'success');
-
-    // Run the compile function
-});
-});
-  /*
-  // Create a BrowserView
-  const browserView = new BrowserView();
-  mainWindow.setBrowserView(browserView);
-  browserView.setBounds({ x: 500, y: 0, width: 700, height: 800 });
-
-  // Load Canvas URL into the BrowserView
-  browserView.webContents.loadURL('https://canvas.pasadena.edu');*/
-
-
-  // Optional: Open DevTools for debugging
-  //browserView.webContents.openDevTools();
 };
 
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow();
-
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
     }
-
-  const win = new BrowserWindow({ width: 800, height: 600 })
-
-  const view = new BrowserView()
-  win.setBrowserView(view)
-  view.setBounds({ x: 0, y: 0, width: 300, height: 300 })
-  view.webContents.loadURL('https://electronjs.org')
   });
 });
 
-// second helper to prevent the app from loosing focus
-// app.on('browser-window-blur', (event, bw) => {
-//   bw.restore()
-//   bw.focus()
-
-//   globalShortcut.unregisterAll();
-// })
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
-    globalShortcut.unregisterAll();
   }
 });
-
-app.on('browser-window-focus', function () {
-  globalShortcut.register("Alt+Tab", () => {
-    // Prevent default behavior
-    // Optionally, we can display a message 
-    showDialog('Alt+Tab is disabled in this application.');
-  });
-  globalShortcut.register("CommandOrControl+R", () => {
-      showDialog("CommandOrControl+V is pressed: Shortcut Disabled");
-  });
-  globalShortcut.register("F5", () => {
-      showDialog("F5 is pressed: Shortcut Disabled");
-  });
-});
-
-app.on('will-quit', function(){
-  globalShortcut.unregisterAll();
-})
-
-
-// Function to display notifications using Electron's dialog module
-function showDialog(message) {
-  dialog.showMessageBox(mainWindow, {
-    type: 'info',
-    message: message,
-    buttons: ['OK']
-  });
-}
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
